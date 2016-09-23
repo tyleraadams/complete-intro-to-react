51e2a51999c79b6ec54e92e08bba1da07690237f
Tyler Adams <tyleraadams@gmail.com>
2016-09-22 22:51:52 -0400

    color prop

--- js/client.js
+++ js/client.js
@@ -5,7 +5,7 @@ var MyTitle = React.createClass({
   render() {
     return(
       div(null,
-        h1(null, this.props.title)
+        h1({style: {color: this.props.color}}, this.props.title)
       )
     )
   }
@@ -14,8 +14,8 @@ var MyTitle = React.createClass({
 var MyTitleFactory = React.createFactory(MyTitle);
 var MyFirstComponent = (
   div(null,
-    MyTitleFactory({title: 'Props are great'}),
-    React.createElement(MyTitle, {title: 'use your props like a big boy'})
+    MyTitleFactory({title: 'Props are great', color: 'peru'}),
+    React.createElement(MyTitle, {title: 'use your props like a big boy', color: 'mediumaquamarine'})
   )
 )
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
