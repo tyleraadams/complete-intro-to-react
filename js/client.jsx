afc1eb6fe929a65b1821ecae080ce588d63594a7
Tyler Adams <tyleraadams@gmail.com>
2016-09-23 14:09:58 -0400

    react router intro

--- js/client.jsx
+++ js/client.jsx
@@ -1,15 +1,13 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-
+const Landing = require('./Landing')
+const ReactRouter = require('react-router')
+const { Router, Route, hashHistory } = ReactRouter
 // stateless componenet
 const App = () => (
-  <div className='app-container'>
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'> or Browse All</button>
-    </div>
-  </div>
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
