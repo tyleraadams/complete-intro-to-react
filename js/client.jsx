4b5bd51918c4ebe18a633b969ba7c89afc0160fe
Tyler Adams <tyleraadams@gmail.com>
2016-09-23 13:33:02 -0400

    functionless component

---
+++ js/client.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const MyTitle = require('./MyTitle')
+
+// stateless componenet
+const MyFirstComponent = function () {
+  return (
+    <div>
+      <MyTitle title='Whatevs' color='rebeccapurple' />
+      <MyTitle title='LOL' color='papayawhip' />
+      <MyTitle title='OMG OMG' color='#f06d06' />
+    </div>
+  )
+}
+
+ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
