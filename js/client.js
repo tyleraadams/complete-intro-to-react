3ef8bf5f938d9267c2fe75edc8619b3e9b1fc6ff
Tyler Adams <tyleraadams@gmail.com>
2016-09-23 12:51:39 -0400

    modules

--- js/client.js
+++ js/client.js
@@ -1,17 +1,8 @@
-/* globals React ReactDOM */
+var React = require('react')
+var  ReactDOM = require('react-dom')
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
-
+var MyTitle = require('./MyTitle')
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
@@ -20,4 +11,5 @@ var MyFirstComponent = (
     React.createElement(MyTitle, {title: 'use your props like a big boy', color: 'mediumaquamarine'})
   )
 )
+
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
